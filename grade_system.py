9190a3b51455190baeed102044b750b6d0a530dc
Sparsh566 <1774.sparsh@gmail.com>
2025-02-03 10:33:31 +0530

    Add files via upload

---
+++ grade_system.py
@@ -0,0 +1,10 @@
+# grading system
+marks = int(input('Enter Marks:'))
+if(marks>85) :
+  print('Grade A')
+elif(marks>60) :
+  print('Grade B')
+elif(marks>40) :
+  print('Grade C')
+else :
+  print('Grade D')
